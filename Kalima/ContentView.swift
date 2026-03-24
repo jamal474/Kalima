5cd4c933c3ecf1fc38f88608e4a246549a1f91b5
Md Shabbir Jamal <mdshabbir820141@gmail.com>
2026-03-24 22:43:48 +0530

    Initial Commit

---
+++ Kalima/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kalima
+//
+//  Created by Md Shabbir Jamal on 24/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kalima/KalimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalimaApp.swift
+//  Kalima
+//
+//  Created by Md Shabbir Jamal on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KalimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
